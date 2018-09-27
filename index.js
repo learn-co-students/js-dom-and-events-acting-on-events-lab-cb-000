d7f3fa8b7d88a8a52b34c1b2c1232029188505c2
Kirby Campbell <jkirbycampbell@gmail.com>
2018-09-27 18:10:40 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,33 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+let input = document.querySelector('input');
+
+function retrieveEmployeeInformation(){
+  return input.value;
+}
+
+function addNewElementAsLi(){
+  let employeeName =  retrieveEmployeeInformation();
+  let employeeList = document.querySelector('.employee-list');
+  employeeList.insertAdjacentHTML('beforeend', `<li>${employeeName}</li>`)
+}
+
+function addNewLiOnClick(){
+  let submit = document.querySelector('input[type="submit"]');
+  submit.addEventListener('click', function(event){
+    addNewElementAsLi()
+    document.querySelector('input').value = '';
+  })
+}
+
+
+
+function clearEmployeeListOnLinkClick(){
+  let clear = document.querySelector('a')
+  let ul = document.querySelector('ul')
+  clear.addEventListener('click', function(event){
+    ul.innerHTML = ''
+  })
+}
