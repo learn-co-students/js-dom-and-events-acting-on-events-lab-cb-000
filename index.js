b170fb52387d63ad916274b0e109bbf97161e3cf
derrick b <dbozich1@gmail.com>
2018-10-10 03:53:34 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,36 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation(){
+  return document.querySelector("input[name='name']").value.trim();
+}
+
+function addNewElementAsLi(){
+  let list = document.querySelector('.employee-list');
+  let li = document.createElement("li");
+  li.innerText = retrieveEmployeeInformation();
+  list.appendChild(li);
+}
+
+function addNewLiOnClick(){
+  let button = document.querySelector("input[type='submit']");
+  button.addEventListener("click", () => {
+    addNewElementAsLi();
+    document.querySelector("input[name='name']").value = ''
+  });
+}
+
+function clearEmployeeListOnLinkClick(){
+  let anchor = document.querySelector('a');
+  anchor.addEventListener('click', () => {
+    let list = document.querySelector('.employee-list');
+
+    while (list.children.length != 0) {
+      list.removeChild(list.children[0]);
+    }
+    list.innerHTML = '';
+
+  });
+
+}
