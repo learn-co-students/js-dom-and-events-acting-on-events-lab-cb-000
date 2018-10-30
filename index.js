f618dab97dbe487dedcd7fe0e8499b3d8610db88
Chris Cardle <cdcardle@gmail.com>
2018-10-30 16:11:34 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,32 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+let input = document.querySelector('input[name="name"]');
+let ul = document.querySelector('.employee-list');
+
+function retrieveEmployeeInformation() {
+  return input.value;
+}
+
+function addNewElementAsLi() {
+  let input = retrieveEmployeeInformation();
+  let li = document.createElement('li');
+  li.appendChild(document.createTextNode(input));
+  ul.appendChild(li);
+}
+
+function addNewLiOnClick() {
+  let submit = document.querySelector('input[type="submit"]')
+  submit.addEventListener("click", () => {
+    addNewElementAsLi();
+    input.value = ''
+  });
+}
+
+function clearEmployeeListOnLinkClick() {
+  let clearButton = document.querySelector('a');
+  return clearButton.addEventListener('click', () => {
+    ul.innerHTML = '';
+  });
+}
