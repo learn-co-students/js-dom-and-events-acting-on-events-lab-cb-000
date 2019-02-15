a215c096e19fe550ed90a59c9c22469b0221c0f4
Adam Urbina <adamurbina@gmail.com>
2019-02-15 17:13:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 function preventRefreshOnSubmit(){
     document.querySelector('form').addEventListener('submit', function(event){
       event.preventDefault()
@@ -5,3 +6,23 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  return document.querySelector('input').value;
+};
+
+function addNewElementAsLi() {
+  const employee = retrieveEmployeeInformation();
+  document.querySelector('ul').append(employee);
+  document.querySelector('input').value = '';
+}
+
+function addNewLiOnClick() {
+  document.addEventListener('click',addNewElementAsLi);
+}
+
+function clearEmployeeListOnLinkClick() {
+  document.querySelector('a').addEventListener('click', function () {
+    document.querySelector('ul').innerHTML='';
+  });
+}
