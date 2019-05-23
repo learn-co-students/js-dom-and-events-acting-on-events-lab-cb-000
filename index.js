de2f30c954a13765a520b5d952837fd47cd5e7fe
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-05-23 17:48:45 +0000

    Done.

--- index.js
+++ index.js
@@ -3,5 +3,23 @@ function preventRefreshOnSubmit(){
       event.preventDefault()
     })
 }
-
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  return $('input[name="name"]').get(0).value;
+}
+function addNewElementAsLi() {
+  $('.employee-list').append(`<li>${retrieveEmployeeInformation()}</li>`);
+}
+function addNewLiOnClick() {
+  addNewElementAsLi();
+  $('input[name="name"]').get(0).value = "";
+}
+function clearList() {
+  $('.employee-list').get(0).innerHTML = '';
+}
+function clearEmployeeListOnLinkClick() {
+  $('a[href="#"]').get(0).addEventListener('click', clearList);
+}
+
+$('input[type="submit"]').get(0).addEventListener('click', addNewLiOnClick);
